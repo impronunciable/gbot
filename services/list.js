318f3796a5020aa47cb54af97af4201b09882cfa
Dan Zajdband <dan.zajdband@gmail.com>
2013-02-11 17:55:18 -0300

    fix on names

--- services/list.js
+++ services/list.js
@@ -4,7 +4,7 @@ module.exports = function(str, fn) {
   var txt = '';
   for(var i in services) {
     el = services[i];
-    txt += el.name + ': ' + el.desc + '\n\n';
+    txt += i + ': ' + el.desc + '\n\n';
   }
   fn('Available commands:\n\n' + txt);
 };
