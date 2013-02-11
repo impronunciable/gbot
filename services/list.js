362ce2a53ff1437951ad79f169305aab227c671d
Dan Zajdband <dan.zajdband@gmail.com>
2013-02-11 17:53:12 -0300

    missing var declaration

--- services/list.js
+++ services/list.js
@@ -1,6 +1,7 @@
 var services = require('../config').services;
 
 module.exports = function(str, fn) {
+  var txt = '';
   for(var i in services) {
     el = services[i];
     txt += el.name + ': ' + el.desc + '\n\n';
